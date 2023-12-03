4c999ed3903ef2eac02c6650b31a6c0e5e55456e
Ben6jamin <b.ishimwe1@alustudent.com>
2023-12-03 16:22:20 +0200

    new

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(cors(
   {
-      origin: ["https://artfulrwanda-frontend.vercel.app/"],
+      origin: ["https://artfulrwanda-frontend.vercel.app"],
       methods: ["POST", "GET"],
       credentials: true
   }
