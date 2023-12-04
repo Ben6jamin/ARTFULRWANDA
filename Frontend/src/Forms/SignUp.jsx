dbc6c8926ad84fcba6f16ee439e24f09cc077068
Ben6jamin <b.ishimwe1@alustudent.com>
2023-12-04 02:03:55 +0200

    gg

--- Frontend/src/Forms/SignUp.jsx
+++ Frontend/src/Forms/SignUp.jsx
@@ -20,7 +20,7 @@ function SignUp() {
   axios.defaults.withCredentials = true;
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post("https://artfulrwanda-backend.vercel.app/register", {name, email, password})
+    axios.post("https://artfulrwanda-backend.vercel.app/", {name, email, password})
     .then(res => {console.log(res)
     navigate('/login') })
     .catch(err => {console.log(err)})
