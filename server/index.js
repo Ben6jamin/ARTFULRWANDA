d5398d95a472fa038bcbadc4f6bcd64cf1440283
Ben6jamin <b.ishimwe1@alustudent.com>
2023-12-04 02:00:29 +0200

    sav

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors(
   {
-      origin: ["https://deploy-mern-frontend.vercel.app"],
+      origin: ["https://artfulrwanda-frontend.vercel.app"],
       methods: ["POST", "GET"],
       credentials: true
   }
